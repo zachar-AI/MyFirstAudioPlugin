04a72fa3f542374991d2e8635554de89b5081732
zachar-AI <zachskihill@gmail.com>
2026-01-06 21:36:24 -0500

    playing with the gui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,4 +1,5 @@
 #include "PluginProcessor.h"
+#include "juce_graphics/juce_graphics.h"
 #include "PluginEditor.h"
 
 //==============================================================================
@@ -8,7 +9,7 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     juce::ignoreUnused (processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (1000, 600);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -19,10 +20,10 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (juce::Colours::black);
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
+    g.setColour (juce::Colours::red);
+    g.setFont (30.0f);
     g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
