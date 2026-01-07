1848d2a6bb289a6c3430189f6864d1d507736ef0
zachar-AI <zachskihill@gmail.com>
2026-01-07 14:57:48 -0500

    we are now using a SmoothedValue for freq. unfortunateley there are still some artifacts

--- Source/SineWave.cpp
+++ Source/SineWave.cpp
@@ -6,6 +6,8 @@ void SineWave::prepare(const double sampleRate, const int numChannels)
   sampleRate_ = static_cast<float>(sampleRate);
   timeIncrement_ = 1.0f / sampleRate_;
   currentTime_.resize(static_cast<size_t>(numChannels), 0.0f);
+  smoothedFreq_.reset(sampleRate_, 1.0f);
+  smoothedFreq_.setCurrentAndTargetValue(getFrequency());
 }
 
 void SineWave::process(juce::AudioBuffer<float> &buffer) {
@@ -18,8 +20,8 @@ void SineWave::process(juce::AudioBuffer<float> &buffer) {
 
     for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
     {
-      output[sample] =
-          amplitude_ * std::sinf(doublePi * frequency_ * currentTime_[static_cast<size_t>(channel)]);
+      float frequency = smoothedFreq_.getNextValue();
+      output[sample] = amplitude_ * std::sinf(doublePi * frequency * currentTime_[static_cast<size_t>(channel)]);
       currentTime_[static_cast<size_t>(channel)] += timeIncrement_;
     }
   }
--- Source/SineWave.h
+++ Source/SineWave.h
@@ -8,16 +8,16 @@ class SineWave
 public:
   void prepare (const double sampleRate, const int numChannels);
   void process (juce::AudioBuffer<float>& buffer);
-  [[nodiscard]] float getAmplitude() const {return amplitude_;}
-  [[nodiscard]] float getFrequency() const {return frequency_;}
-  void setAmplitude(const float amplitude) {amplitude_ = amplitude;}
-  void setFrequency(const float frequency) {frequency_ = frequency;}
+  [[nodiscard]] float getAmplitude() const { return amplitude_; }
+  [[nodiscard]] float getFrequency() { return smoothedFreq_.getNextValue(); }
+  void setAmplitude(const float amplitude) { amplitude_ = amplitude; }
+  void setFrequency(const float frequency) { smoothedFreq_.setTargetValue(frequency); }
 
 private:
   std::vector<float> currentTime_;
-  float frequency_ = 50.0f;
   float amplitude_ = 0.5f;
   float sampleRate_ = 0.0f;
   float timeIncrement_ = 0.0f;
   static constexpr float doublePi = 2.0f * std::numbers::pi_v<float>;
+  juce::SmoothedValue<float, juce::ValueSmoothingTypes::Multiplicative> smoothedFreq_;
 };
